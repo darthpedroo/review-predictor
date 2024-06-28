d7d2da9280bcb639eb079cad55757b9cfecccb1c
darthpedroo <pedrovillarino06@gmail.com>
2024-06-27 21:34:02 -0300

    minor fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <header>
 
     MOVIE REVIEW
+    <HomeView></HomeView>
     
   </header>
 
